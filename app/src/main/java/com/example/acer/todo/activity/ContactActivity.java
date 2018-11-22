c5c1755cd13881a17ed2f291c2944be6c3c0af77
SumonZ <635176614@qq.com>
2018-11-22 22:57:10 +0800

    实现数据传输

--- app/src/main/java/com/example/acer/todo/activity/ContactActivity.java
+++ app/src/main/java/com/example/acer/todo/activity/ContactActivity.java
@@ -5,6 +5,7 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothServerSocket;
 import android.bluetooth.BluetoothSocket;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -22,7 +23,9 @@ import android.support.v7.widget.RecyclerView;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -40,6 +43,8 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.util.List;
 import java.util.UUID;
 
@@ -48,7 +53,7 @@ import butterknife.ButterKnife;
 import com.example.acer.todo.constants.MyMessageViewType;
 
 public class ContactActivity extends AppCompatActivity implements View.OnClickListener,
-        RvDeviceStateAdapter.DataSocketUiUpdateListener,BlueToothReceiver.FinishTransformUpdateListener {
+        RvDeviceStateAdapter.DataSocketUiUpdateListener,BlueToothReceiver.FinishTransformUpdateListener{
 
     private static final String TAG = "ContactActivity";
 
@@ -60,6 +65,8 @@ public class ContactActivity extends AppCompatActivity implements View.OnClickLi
     private String tempSendingMessage="";
     private ClientConnectThread mClientConnectThread;
     private ServAcceptThread mServAcceptThread;
+    private BluetoothSocket clientBluetoothSocket;
+    private BluetoothServerSocket serverSocket;
 
     @BindView(R2.id.close)
     public Button closeBt;
@@ -82,7 +89,7 @@ public class ContactActivity extends AppCompatActivity implements View.OnClickLi
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
-        popDialog();
+
         ButterKnife.bind(this);
         checkPermission();
         initUI();
@@ -116,31 +123,21 @@ public class ContactActivity extends AppCompatActivity implements View.OnClickLi
         deviceStateAdapter=new RvDeviceStateAdapter(this);
         rvDevices.setAdapter(deviceStateAdapter);
         messageAdapter=new RvMessageAdapter(this);
+//        当时忘记写了  通过debug  会提示错误
+        LinearLayoutManager linearLayoutManager=new LinearLayoutManager(this);
+//        设置自动 滚到 最下面
+        linearLayoutManager.setStackFromEnd(true);
+        rvMessages.setLayoutManager(linearLayoutManager);
         rvMessages.setAdapter(messageAdapter);
         openBt.setOnClickListener(this);
         closeBt.setOnClickListener(this);
         searchBt.setOnClickListener(this);
-        editTv.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-
-            }
-
-            @Override
-            public void afterTextChanged(Editable s) {
-                tempSendingMessage=s.toString();
-            }
-        });
         deviceStateAdapter.setDataSocketUiUpdateListener(this);
+        sendBt.setOnClickListener(this);
 
-        if (mServAcceptThread!=null){
-            rvDevices.setClickable(false);
-        }
+        rvDevices.setClickable(false);
+
+
 ////        int groupId ,  int  itemId, int order,  chars title
 //        navigationView.getMenu().add(1,1,1,"hhh");
 //        navigationView.getMenu().add(1,2,2,"aaa");
@@ -159,8 +156,11 @@ public class ContactActivity extends AppCompatActivity implements View.OnClickLi
                             case 1:
                                 mServAcceptThread=new ServAcceptThread();
                                 mServAcceptThread.start();
+                                Log.d(TAG, "onClick: 1");
                                 break;
                             case 0:
+                                Log.d(TAG, "onClick: 0");
+                                rvDevices.setClickable(true);
                                 break;
                         }
                     }
@@ -185,12 +185,14 @@ public class ContactActivity extends AppCompatActivity implements View.OnClickLi
                     Intent intent=new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
                     startActivityForResult(intent,requestOpenBlueTooth);
                 }
+                popDialog();
                 break;
             case R.id.search:
                 blueToothAdapter.startDiscovery();
                 registBlueToothReceiver();
                 break;
             case R.id.send:
+                tempSendingMessage=editTv.getText().toString();
                 CommunicationMessage communicationMessage=new CommunicationMessage();
                 communicationMessage.setMessage(tempSendingMessage);
                 communicationMessage.setType(MyMessageViewType.SEND);
@@ -199,6 +201,7 @@ public class ContactActivity extends AppCompatActivity implements View.OnClickLi
                     mClientConnectThread.sendMessage(tempSendingMessage);
                 }else
                     mServAcceptThread.sendMessage(tempSendingMessage);
+                editTv.getText().clear();
                 break;
         }
     }
@@ -233,6 +236,7 @@ public class ContactActivity extends AppCompatActivity implements View.OnClickLi
         }
     }
 
+
 //    耗时  操作 放在拎一个线程
 
     public class ClientConnectThread extends Thread{
@@ -244,6 +248,7 @@ public class ContactActivity extends AppCompatActivity implements View.OnClickLi
 
         private InputStream inputStream;
         private OutputStream outputStream;
+        private PrintWriter printWriter;
 
         public ClientConnectThread(BluetoothDevice device){
             this.device=device;
@@ -254,39 +259,68 @@ public class ContactActivity extends AppCompatActivity implements View.OnClickLi
         public void run() {
             if (device!=null){
                 try{
-                    BluetoothSocket bluetoothSocket=device.createInsecureRfcommSocketToServiceRecord(UUID.fromString(UUIDstr));
-                    if (bluetoothSocket!=null){
+                    BluetoothSocket temp=device.createInsecureRfcommSocketToServiceRecord(UUID.fromString(UUIDstr));
+                    clientBluetoothSocket=temp;
+                    if (clientBluetoothSocket!=null){
 //                        链接 不需要端口ip？  或者其他
-                        bluetoothSocket.connect();
+                        clientBluetoothSocket.connect();
+                        Log.d(TAG, "run: 连接到 服务端 connect ");
+
                     }
-
-                    inputStream=bluetoothSocket.getInputStream();
-                    outputStream=bluetoothSocket.getOutputStream();
-
-                    BufferedReader reader=new BufferedReader(new InputStreamReader(inputStream,"UTF-8"));
-
                     String content=null;
-                    while (canConnect){
-                        content=reader.readLine();
+                    try{
+                        inputStream=clientBluetoothSocket.getInputStream();
+                        outputStream=clientBluetoothSocket.getOutputStream();
+
+                        BufferedReader reader=new BufferedReader(new InputStreamReader(inputStream,"UTF-8"));
+
+                        while ((content=reader.readLine())!=null&&content.length()!=0){
+                            Log.d(TAG, "run: 收到消息："+content);
+                            final CommunicationMessage recvednMessage=new CommunicationMessage();
+                            recvednMessage.setMessage(content);
+                            recvednMessage.setType(MyMessageViewType.RECV);
+                            ContactActivity.this.runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    messageAdapter.addMessage(recvednMessage);
+                                }
+                            });
+                        }
+
+                    }catch (IOException e){
+                        e.printStackTrace();
                     }
+
 //                    这样的话，每次发送接受数据 都会创建实例， 每条信息 对应一个 对象
-                    CommunicationMessage recvednMessage=new CommunicationMessage();
-                    recvednMessage.setMessage(content);
-                    recvednMessage.setType(MyMessageViewType.RECV);
-                    messageAdapter.addMessage(recvednMessage);
 
                 }catch (IOException e){
                     e.printStackTrace();
+                }finally {
+                    try{
+                        if (clientBluetoothSocket!=null){
+                            clientBluetoothSocket.close();
+                        }
+                    }catch (IOException e){
+                        e.printStackTrace();
+                    }
+
                 }
             }
         }
         public void sendMessage(String message){
             if (outputStream!=null){
 
+//                神了 我去 ，， outputStream.write 对面是接受不到的
                 try{
-                    outputStream.write(message.getBytes());
+                    if (printWriter==null){
+                        printWriter=new PrintWriter(new OutputStreamWriter(outputStream,"UTF-8"),true);
+                    }
+                    printWriter.println(message);
+                    Log.d(TAG, "sendMessage: 已经写入 字节流");
                 }catch (IOException e){
                     e.printStackTrace();
+                    printWriter.close();
+                    Log.d(TAG, "sendMessage: "+e.getMessage());
                 }
             }
 
@@ -299,47 +333,111 @@ public class ContactActivity extends AppCompatActivity implements View.OnClickLi
         private InputStream inputStream;
         private BluetoothSocket socket;
         private boolean canAccept;
+        private PrintWriter printWriter;
 
         public ServAcceptThread(){
             canAccept=true;
         }
         @Override
         public void run() {
+
+            if (blueToothAdapter==null)
+
+                return;
+            Log.d(TAG, "run: blueToothAdapter不为空");
             try{
 //               核心
-                BluetoothServerSocket serverSocket=blueToothAdapter.
+                BluetoothServerSocket temp=blueToothAdapter.
                         listenUsingInsecureRfcommWithServiceRecord("Mon",UUID.fromString(UUIDstr));
+                serverSocket=temp;
+
                 if (serverSocket!=null){
 //                    核心2
-                    socket=serverSocket.accept();
-                    deviceStateAdapter.addMessage("有客户端链接");
 
+                    Log.d(TAG, "run: 111");
+
+                    socket=serverSocket.accept();
+                    if (socket==null){
+                        Log.d(TAG, "run:服务端未接受到 设备连接");
+                    }
+
+                    Log.d(TAG, "run: 接受到");
+//                    这个需要在主线程中更新的   在这纠结了 一上午  没找着
+                    ContactActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            deviceStateAdapter.addMessage("有客户端链接");
+
+                        }
+                    });
                     outputStream=socket.getOutputStream();
                     inputStream=socket.getInputStream();
 
-                    BufferedReader reader=new BufferedReader(new InputStreamReader(inputStream,"UTF-8"));
-                    String content=null;
-                    while (canAccept){
-                        content=reader.readLine();
+                    if (inputStream!=null){
+                        BufferedReader reader=new BufferedReader(new InputStreamReader(inputStream,"UTF-8"));
+                        String content=null;
+
+                        while ((content=reader.readLine())!=null&&content.length()!=0){
+                            Log.d(TAG, "run: 收到消息："+content);
+                            final CommunicationMessage recvednMessage=new CommunicationMessage();
+                            recvednMessage.setMessage(content);
+                            recvednMessage.setType(MyMessageViewType.RECV);
+                            ContactActivity.this.runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    messageAdapter.addMessage(recvednMessage);
+                                }
+                            });
+                        }
+
                     }
-                    CommunicationMessage recvednMessage=new CommunicationMessage();
-                    recvednMessage.setMessage(content);
-                    recvednMessage.setType(MyMessageViewType.RECV);
-                    messageAdapter.addMessage(recvednMessage);
                 }
             }catch (IOException e){
                 e.printStackTrace();
+                Log.d(TAG, "run: "+e.toString());
+            }finally {
+                try{
+                    if (serverSocket!=null){
+                        serverSocket.close();
+                    }
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
             }
         }
 
         public void sendMessage(String message){
             if (outputStream!=null){
                 try{
-                    outputStream.write(message.getBytes());
+                    if (printWriter==null){
+                        printWriter=new PrintWriter(new OutputStreamWriter(outputStream,"UTF-8"),true);
+                    }
+                    printWriter.println(message);
                 }catch (IOException e){
                     e.printStackTrace();
                 }
             }
         }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(blueToothReceiver);
+        blueToothAdapter.disable();
+    }
+
+//    每次点击  都会判断是否在 editText 内 ，判断是否需要 收起软键盘
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        if (ev.getAction()==MotionEvent.ACTION_DOWN){
+            View v=getCurrentFocus();
+        }
+        return super.dispatchTouchEvent(ev);
+    }
+
+    public boolean isShouldHideEditText(){
+
+        return true;
     }
 }
